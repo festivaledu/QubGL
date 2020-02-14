0debd04fb6459610999d8c8e21d6316ff3cce765
Väinämö Łūmikērø <vainamo@git.festival.ml>
2020-02-14 16:17:48 +0100

    Finish model

---
+++ QubGL/src/Rendering/Model.cpp
@@ -0,0 +1,19 @@
+#include "Model.hpp"
+
+#include "CubeMesh.hpp"
+#include "ShaderProgram.hpp"
+
+Model::Model(const CubeMesh& mesh)
+	:m_mesh(mesh) { }
+
+Model::~Model() { }
+
+void Model::Draw(const ShaderProgram& shader) {
+	shader.Bind();
+	shader.SetModelMatrix(*this);
+	m_mesh.Draw();
+}
+
+Transform& Model::GetTransform() {
+	return m_transform;
+}
