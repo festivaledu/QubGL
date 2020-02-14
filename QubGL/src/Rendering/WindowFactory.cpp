6763cfd74fe4fc9a0a3ca686038537265d4cb8cd
Väinämö Łūmikērø <vainamo@git.festival.ml>
2020-02-14 16:17:36 +0100

    Finish window factory

--- QubGL/src/Rendering/WindowFactory.cpp
+++ QubGL/src/Rendering/WindowFactory.cpp
@@ -1,6 +1,8 @@
 #include "WindowFactory.hpp"
 
+#include "OpenGlWindow.hpp"
+
 Window* WindowFactory::CreateWindow(const std::string& title, unsigned int width, unsigned int height)
 {
-	
+	return new OpenGlWindow(title, width, height);
 }
