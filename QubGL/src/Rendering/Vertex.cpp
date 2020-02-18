e1d45d1b77a98558fc6357d65a854e28a5e2f071
Väinämö Łūmikērø <vainamo@git.festival.ml>
2020-02-18 22:15:48 +0100

    Add random color to each face for better distinguishability

--- QubGL/src/Rendering/Vertex.cpp
+++ QubGL/src/Rendering/Vertex.cpp
@@ -1,4 +1,8 @@
 #include "Vertex.hpp"
 
+#include <random>
+
 Vertex::Vertex(glm::vec3 position, glm::vec4 color, glm::vec3 normal)
-    :Position(position), Color(color), Normal(normal) { }
+    :Position(position), Color(color), Normal(normal) {
+    Color = glm::vec4((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, 1.F);
+}
